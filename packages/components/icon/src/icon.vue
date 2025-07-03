a063023bbe441dc618541ea63141afb7b54e7621
tomato <1710151546@qq.com>
2025-07-03 17:34:49 +0800

    init

---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,15 @@
+<template>
+    <i :class="bem.b()">
+        <slot></slot>
+    </i>
+</template>
+<script setup lang="ts">
+import { createNameSpace } from '@zi-shui/utils/create'
+import { iconProps } from './icon'
+
+const bem = createNameSpace('icon')
+
+const props = defineProps(iconProps)
+
+//计算属性 来计算一个样式来处理
+</script>
